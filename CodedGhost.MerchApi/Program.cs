959894782deae58dd6993000387cdbd1eadacec2
codedghost2 <codedghost2@gmail.com>
2022-10-20 23:15:20 +0100

    Initial setup

--- CodedGhost.MerchApi/Program.cs
+++ CodedGhost.MerchApi/Program.cs
@@ -6,32 +6,12 @@ namespace CodedGhost.MerchApi
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: true)
+                .Build();
 
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            CreateHostBuilder(args, config).Run();
         }
 
         public static IHost CreateHostBuilder(string[] args, IConfigurationRoot config) =>
--- CodedGhost.MerchApi/Startup.cs
+++ CodedGhost.MerchApi/Startup.cs
@@ -1,8 +1,15 @@
 ﻿using CodedGhost.Config;
 using CoreCodedChatbot.Config;
 using CoreCodedChatbot.Database;
+using CoreCodedChatbot.Database.Context.Interfaces;
+using CoreCodedChatbot.Database.Context;
 using CoreCodedChatbot.Logging;
 using CoreCodedChatbot.Secrets;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
 
 namespace CodedGhost.MerchApi;
 
@@ -29,10 +36,35 @@ public class Startup
             .AddDbContextFactory()
             .AddChatbotNLog()
             .AddPrintfulClient(secretService);
+
+        services.AddRouting();
+
+        services.AddControllers().AddNewtonsoftJson(x =>
+            x.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
     }
 
     public void Configure(IApplicationBuilder app, IHostEnvironment env, IServiceProvider serviceProvider)
     {
+        using (var context = (ChatbotContext)serviceProvider.GetService<IChatbotContextFactory>().Create())
+        {
+            context.Database.Migrate();
+        }
 
+        app.UseMiddleware<ErrorHandlingMiddleware>();
+
+        if (env.IsDevelopment() || env.IsEnvironment("Local"))
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapDefaultControllerRoute();
+        });
     }
 }
